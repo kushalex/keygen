765627899bcb1728619ba12dce4a5ddb87d3bb94
Alex <Alex@DESKTOP-ROQ3TH1>
2024-07-09 22:52:20 -0500

    Add project files.

---
+++ zarkey/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿using System.Windows.Forms;
+
+namespace zarkey
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            panel1 = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(70, 203);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Enter Username";
+            textBox1.Size = new Size(260, 23);
+            textBox1.TabIndex = 4;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(70, 276);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(260, 23);
+            textBox2.TabIndex = 5;
+            textBox2.Text = "< TEAM 3RDWRLD >";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(70, 247);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(260, 23);
+            textBox3.TabIndex = 6;
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(50, 170);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(300, 180);
+            panel1.TabIndex = 8;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(200, 135);
+            button3.Name = "button3";
+            button3.Size = new Size(80, 25);
+            button3.TabIndex = 13;
+            button3.Text = "Exit";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(110, 135);
+            button2.Name = "button2";
+            button2.Size = new Size(80, 25);
+            button2.TabIndex = 12;
+            button2.Text = "About";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(20, 135);
+            button1.Name = "button1";
+            button1.Size = new Size(80, 25);
+            button1.TabIndex = 11;
+            button1.Text = "Generate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(17, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Log :";
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(17, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Username :";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = ZARKEY.Properties.Resources.fllogo1;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(288, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 100);
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoValidate = AutoValidate.EnableAllowFocusChange;
+            BackColor = SystemColors.WindowFrame;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(400, 400);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(panel1);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ZARKEY";
+            Load += Form1_Load;
+            MouseDown += mouse_Down;
+            MouseMove += mouse_Move;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ zarkey/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System.Media;
+using System.Text;
+using System.Windows.Forms;
+
+namespace zarkey
+{
+    public partial class Form1 : Form
+    {
+        public Point mouseLocation;
+        private SoundPlayer soundPlayer;
+        public Form1()
+        {
+            InitializeComponent();
+            soundPlayer = new SoundPlayer(ZARKEY.Properties.Resources.zarkey);
+            soundPlayer.PlayLooping();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = panel1;
+            panel1.BackColor = Color.FromArgb(200, 0, 0, 0);
+        }
+
+        // "Generate" button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please enter a username.", "Username Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox3.Text = "";
+                return;
+            }
+
+            string userInput = textBox1.Text;
+            StringBuilder asciiBuilder = new StringBuilder();
+            foreach (char c in userInput)
+            {
+                // char to ASCII
+                asciiBuilder.Append(((int)c).ToString() + " ");
+            }
+
+            // set modifiedText in textBox3
+            string modifiedText = asciiBuilder.ToString().Trim(); // remove trailing space
+            textBox3.Text = modifiedText;
+        }
+
+        // "Exit" button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("It's watching you now.", "", MessageBoxButtons.OK);
+            Application.Exit();
+        }
+
+        // move application on click drag
+        private void mouse_Down(object sender, MouseEventArgs e)
+        {
+            mouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void mouse_Move(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouseLocation.X, mouseLocation.Y);
+                Location = mousePos;
+            }
+        }
+
+        // open form2 when user clicks "About" button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+    }
+}
